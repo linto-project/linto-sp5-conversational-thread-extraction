94ee111c068fc65d25341904ef903aeca035dd69
Philippe Muller <philippe.muller@irit.fr>
2020-05-19 17:55:51 +0200

    modified:   seq_of_seq_encoder.py

--- experiments/seq_of_seq_encoder.py
+++ experiments/seq_of_seq_encoder.py
@@ -1,6 +1,8 @@
+import itertools
 import logging
 from typing import Dict, List, Iterable
 import torch
+import torch.optim as optim
 
 #from allennlp.data.dataset_readers import SnliReader
 from allennlp.data.dataset_readers.dataset_reader import DatasetReader
@@ -10,15 +12,17 @@ from allennlp.data.fields import Field
 from allennlp.data.fields import LabelField
 from allennlp.data.fields import TextField, ListField
 from allennlp.data.instance import Instance
-from allennlp.data.token_indexers import TokenIndexer
+from allennlp.data.token_indexers import TokenIndexer, SingleIdTokenIndexer
 from allennlp.data.tokenizers import Tokenizer
 from allennlp.modules.seq2vec_encoders import BertPooler
+from allennlp.modules import TextFieldEmbedder
+
 # should be a better way
 from allennlp.modules.stacked_bidirectional_lstm import StackedBidirectionalLstm
-from allennlp.models.model import Model
+from allennlp.modules import Seq2VecEncoder
 from allennlp.training.metrics import CategoricalAccuracy
 from allennlp.nn.util import get_text_field_mask
-
+from allennlp.data.vocabulary import Vocabulary
 
 
 from overrides import overrides
@@ -86,12 +90,25 @@ class ChatReader(DatasetReader):
         lines_field = ListField([TextField(tokenized_line,self._token_indexers) for tokenized_line in inst_tokens])
         # labels are head index for each turn; root is "root"
         target = ListField([LabelField(label=head) for idx,head in inst_deps])  # or simply LabelField
-        instance = Instance({'lines': lines_field, 'labels': target})
+        #instance = Instance({'lines': lines_field, 'labels': target})
         # mock label for testing of model as classification of a chat ; force label = 0
         instance = Instance({'lines': lines_field, 'labels': 0})
         return instance
 
+###### VERY IMPORTANT: the name fields have to be found in the forward method of the models
+#
+#
+#  (from the tutorial with a model classifying papers)
+#       "The first thing to notice are the inputs to the method. Remember the DatasetReader we implemented? It created Instances with
+#fields named title, abstract, and label. That's where the names to forward come from - they have to match the names that we gave to the
+# fields in our DatasetReader"
 
+
+
+
+
+
+    
 # tokenized_lines: List[List[Token]]
 # lines_field = ListField([TextField(tokenized_line) for tokenized_line in tokenized_lines])
 # target = ListField([LabelField(label=label) for label in labels)  # or simply LabelField
@@ -165,27 +182,31 @@ class ChatReader(DatasetReader):
 """
 
 
-class ChatClassification(Model):
+class TurnMeanEmbedding(Seq2VecEncoder):
+    pass
+
+class ChatClassification(Seq2VecEncoder):
 
 
-    def __init__(self,vocab, pretrained_model: str="bert-base-uncased", requires_grad: bool = True):
-        super(ChatClassification, self).__init__(vocab)
+    def __init__(self, vocab, pretrained_model: str="bert-base-uncased", requires_grad: bool = True):
+        super(ChatClassification, self).__init__()
+        self.vocab = vocab
         self.turn_pooler = BertPooler(pretrained_model,requires_grad,dropout=0.0)
+        #self.turn_pooler = 
         self.chat_encoder = StackedBidirectionalLstm(hidden_size= 400,
                                                      input_size= 768,
                                                      num_layers= 1,
                                                      recurrent_dropout_probability=0.3,
                                                      use_highway=True)
-        self.classif_layer = torch.nn.Linear(in_features=self.chat_encoder.get_output_dim(),
+        self.classif_layer = torch.nn.Linear(in_features=self.chat_encoder.hidden_size,
                                              out_features=2)
         self.accuracy = CategoricalAccuracy()
 
         
     def forward(self,
-                all_turns, 
-                label = None
-            ):
-        turns_embeddings = [self.turn_pooler(turn) for turn in all_turns]
+                lines,
+                label = None):
+        turns_embeddings = [self.turn_pooler(turn) for turn in lines]
         output_seq, final_h, final_c = self.chat_encoder(turns_embeddings)
         logits = self.classif_layer(final_h)
         output = {"tag_logits": logits}
@@ -206,27 +227,91 @@ if __name__=="__main__":
     from allennlp.data.tokenizers import Token, Tokenizer, WordTokenizer, PretrainedTransformerTokenizer
     from allennlp.data.token_indexers import PretrainedTransformerIndexer
     from allennlp.data.vocabulary import Vocabulary
-    #from allennlp.training.trainer import Trainer
-
-
-    # for pretrained model
-    tokenizer = PretrainedTransformerTokenizer(
-        model_name="bert-base-uncased",
-        do_lowercase=True,
-        )
+    from allennlp.training.trainer import Trainer
+    from allennlp.data.iterators import BucketIterator
+    from allennlp.common import Params
     
+    # for bert pretrained model
+    #tokenizer = PretrainedTransformerTokenizer(
+    #    model_name="bert-base-uncased",
+    #    do_lowercase=True,
+    #    )
+    #token_indexers = PretrainedTransformerIndexer(
+    #    model_name="bert-base-uncased",
+    #    do_lowercase=True
+    #    )
 
-    token_indexers = PretrainedTransformerIndexer(
-        model_name="bert-base-uncased",
-        do_lowercase=True
-        )
+    
+    #token_indexer_cfg =  Params({"tokens": {
+    #    "type": "single_id",
+    #    "lowercase_tokens": "true"
+    #}})
+    #token_indexers = TokenIndexer.from_params(token_indexer_cfg.pop("tokens"))
+    token_indexers = {"tokens": SingleIdTokenIndexer()}
+    
+    tokenizer_cfg = Params({"word_splitter": {"language": "en"}})
+
+    tokenizer = Tokenizer.from_params(tokenizer_cfg)
+                                
+    
     reader = ChatReader(
         tokenizer=tokenizer,
         token_indexers=token_indexers,
-        #use_pretrained_model=True
         )
     train_instances = reader.read("./train_dummy.tsv")
-    vocab = Vocabulary.from_instances(train_instances)
+    
+    all_turns = list(itertools.chain(*(x["lines"] for x in train_instances)))
+    vocab = Vocabulary.from_instances(all_turns)
+
+
+    text_field_embedder_cfg = Params({"type": "sequence_encoding",
+                                      "embedding": {
+                                          "embedding_dim": 100
+                                      },
+                                      "encoder": {
+                                          "type": "gru",
+                                          "input_size": 100,
+                                          "hidden_size": 50,
+                                          "num_layers": 2,
+                                          "dropout": 0.25,
+                                          "bidirectional": True
+                                      }                                    
+    })
+
+    
+    #text_field_embedder= TextFieldEmbedder.from_params(vocab,text_field_embedder_cfg)
+
+    
+    trainer_cfg = Params({"iterator": {"type": "basic",
+                                       "batch_size": 32
+    },
+                          "trainer": {
+                              "optimizer": {
+                                  "type": "adam"
+                              },
+                              "num_epochs": 3,
+                              "patience": 10,
+                              "cuda_device": -1
+                          }
+    })
+
+
     model = ChatClassification(vocab)
-    trainer = Trainer(model=model)
-    trainer.train()
+
+    optimizer = optim.SGD(model.parameters(), lr=0.1)
+    iterator = BucketIterator(batch_size=1,sorting_keys=["lines"])
+
+    trainer = Trainer(model=model,
+                      optimizer=optimizer,
+                      iterator=iterator,
+                      train_dataset=train_instances
+    )
+
+
+    if True:
+        i0 = train_instances[0]
+        t0 = i0["lines"][0]
+        all_turns = list(itertools.chain(*(x["lines"] for x in train_instances)))
+        vocab = Vocabulary.from_instances(all_turns)
+
+                                                       
